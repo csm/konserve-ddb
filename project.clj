ea27bc592cbe3b46c6d9ccabfab39d1e619c78a0
Casey Marshall <csm@noonhome.com>
2019-11-07 23:43:27 -0800

    Version 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject konserve-ddb "0.1.0-SNAPSHOT"
+(defproject konserve-ddb "0.1.0"
   :description "FIXME: write description"
   :url "https://github.com/csm/konserve-ddb"
   :license {:name "MIT"
